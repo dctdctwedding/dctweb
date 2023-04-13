122182627664bef53a9a930cda0a86bdc090510c
aquahsu0811 <bellems811@gmail.com>
2023-04-13 23:54:24 +0800

    add animation

--- assets/js/feedback_fb.js
+++ assets/js/feedback_fb.js
@@ -2,14 +2,17 @@ function more() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("btn");
-  
+    let expanded = document.querySelector(".expanded");
+
     if (dots.style.display === "none") {
       dots.style.display = "inline";
       btnText.innerHTML = "Read more"; 
       moreText.style.display = "none";
+      expanded.style.maxHeight  ="130px";
     } else {
       dots.style.display = "none";
       btnText.innerHTML = "Read less"; 
       moreText.style.display = "inline";
+      expanded.style.maxHeight  ="230px";
     }
   }
