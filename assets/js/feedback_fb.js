17a861801c72ce7832f369b71fab3510ff0873ed
aquahsu0811 <bellems811@gmail.com>
2023-04-16 00:01:46 +0800

    add open and collapse animate

--- assets/js/feedback_fb.js
+++ assets/js/feedback_fb.js
@@ -1,18 +1,47 @@
-function more() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("btn");
-  let expanded = document.querySelector(".expanded");
+// function more(id) {
+//   console.log(id)
+//   let idO = document.querySelector("#"+id + ".expanded");
+//   console.log(idO)
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more");
+//   var btnText = document.getElementById("btn");
+//   let expanded = document.querySelector(".expanded");
 
-  if (expanded.classList.contains("active")) {
-    expanded.classList.remove("active");
+//   if (idO.classList.contains("active")) {
+//     expanded.classList.remove("active");
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     moreText.style.display = "none";
+//   } else {
+//     expanded.classList.add("active");
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     moreText.style.display = "inline";
+//   }
+// }
+
+function more(id) {
+  console.log(id)
+  const element = document.getElementById(id);
+  const dots = element.querySelector("#dots");
+  const moreText = element.querySelector(".more");
+  const btnText = element.querySelector(".btn-more");
+
+  
+  console.log("h:",fullHeight)
+
+  if (element.classList.contains("active")) {
+    element.classList.remove("active");
     dots.style.display = "inline";
     btnText.innerHTML = "Read more";
     moreText.style.display = "none";
+    $('#'+id).animate({'height': 120}, 300);
   } else {
-    expanded.classList.add("active");
+    element.classList.add("active");
     dots.style.display = "none";
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
+    var fullHeight = element.querySelector(".block").offsetHeight;
+    $('#'+id).css({'height': 120, 'max-height': 'none'}).animate({'height': fullHeight}, 300);
   }
 }
